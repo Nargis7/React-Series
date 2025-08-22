a4ef3dd9320c21d1ef3d05c5cf349f7e9375a42b
nargis7 <ronaldonargis786@gmail.com>
2025-08-22 16:23:06 +0530

    useEffect

---
+++ 06vite-project/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(1);
+  // useEffect is a hook that allows you to perform side effects in function components
+  // first -> side-effect function
+  // second -> cleanup function
+  // third -> dependencies
+
+  // variation:1
+  // runs on every render
+  // useEffect(()=>{
+  //   alert('Component rendered!')
+  // })
+
+  // variation: 2
+  // runs only on first render
+  // useEffect(()=>{
+  //   alert('Component rendered!')
+  // },[])
+
+  // variation: 3
+  // useEffect(() => {
+  //   alert('Component unmounted!')
+  // },[count])
+
+  // variation: 4
+  // multiple dependencies
+  // useEffect(() => {
+  //   alert('Count changed!')
+  // },[count,total])
+
+
+  // variation: 5
+  // lets add cleanup function
+  useEffect(() => {
+    alert('Component mounted!')
+    return () => {
+      alert('Component unmounted!')
+    }
+  },[count])
+
+  function handleClick(){
+    setCount(count + 1);
+    // setTotal(total + 1);
+  }
+ return (
+  <div>
+    <button onClick={handleClick}>
+      click me
+    </button>
+    <br />
+    Count is : {count}
+    <br />
+    Total is : {total}
+  </div>
+ )
+
+  // useEffect(() =>{
+  //   first
+  //   return () => {
+  //     second
+//     }
+//   },[third])
+  
+//   return (
+//     <div>hi</div>
+//   )
+}
+
+export default App
