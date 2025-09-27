1522ae73ecd9783fd4875a9e8602ea2697b06cc1
nargis7 <ronaldonargis786@gmail.com>
2025-09-27 14:32:06 +0530

    use of hooks

---
+++ 09vite-project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+function App() {
+const[count, setCount] = useState(0)
+
+const addValue = () => {
+  setCount(count + 1);
+}
+
+const removeValue = () => {
+  setCount(count - 1);
+}
+
+  return (
+    <>
+    <h2>Counter value: {count} </h2>
+    <button  onClick={addValue}>
+      Increment Counter
+    </button>
+    <br />
+    <button 
+    onClick={removeValue}
+    >Decrement Counter</button>
+    </>
+  )
+}
+export default App
