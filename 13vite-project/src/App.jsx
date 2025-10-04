b1890df7551dc8024a5852ce925ce0a231fec0e5
nargis7 <ronaldonargis786@gmail.com>
2025-10-04 21:49:54 +0530

    working on react router

---
+++ 13vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/Header/Header.jsx'
+import Home from './components/Home/Home.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Header />
+     <Home />
+     <Footer />
+    </>
+  )
+}
+
+export default App
