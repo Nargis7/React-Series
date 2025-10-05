7fdd89d4c5eb5bd321ffa2014a715f8a81692275
nargis7 <ronaldonargis786@gmail.com>
2025-10-05 19:36:48 +0530

    incomplete

---
+++ 14vite-project/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src=""  />
+      <u>
+        <li>Home</li>
+        <li>About</li>
+        <li>Product</li>
+        <li>Contact</li>
+      </u>
+      <button>Get Started</button>
+    </div>
+  )
+}
+
+export default Navbar
