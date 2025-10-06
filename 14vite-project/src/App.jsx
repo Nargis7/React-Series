b7ab96da47e3490038d37e688f19e5c0ac462bb1
nargis7 <ronaldonargis786@gmail.com>
2025-10-06 18:32:04 +0530

    react -router-concepts

--- 14vite-project/src/App.jsx
+++ 14vite-project/src/App.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Product from './pages/Product'
+import Contact from './pages/Contact' 
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import RootLayout from './layout/RootLayout'
 
 const App = () => {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='product' element={<Product />} />
+          <Route path='contact' element={<Contact />} />
+      </Route>
+    )
+  )
   return (
-    <div>
-      <Navbar />
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
 export default App
+
+
+
+    // <div>
+{/* <Navbar /> */}
+      {/* <div className='container'> */}
+        {/* <Routes> */}
+          {/* <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/contact' element={<Contact />} /> */}
+        {/* </Routes> */}
+         {/* </div> */}
+    // </div>
--- 14vite-project/src/components/Navbar.jsx
+++ 14vite-project/src/components/Navbar.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import  logo  from '../assets/logo.jpeg'
+// import { Link } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <img src=""  />
-      <u>
-        <li>Home</li>
-        <li>About</li>
-        <li>Product</li>
-        <li>Contact</li>
-      </u>
-      <button>Get Started</button>
+    <div className='navbar'>
+      <img src={logo}   width="130px"/>
+      <ul>
+       <NavLink to='/'><li>Home</li></NavLink>
+       <NavLink to='/about'><li>About</li></NavLink>
+       <NavLink to='/product'><li>Product</li></NavLink>
+       <NavLink to='/contact'><li>Contact</li></NavLink>
+      </ul>
+      <button onClick={() => navigate('/contact')}>Get Started</button>
     </div>
   )
 }
-
 export default Navbar
+
+
+// <Link to='/'><li>Home</li></Link>
+      //  <Link to='/about'><li>About</li></Link>
+      //  <Link to='/product'><li>Product</li></Link>
+      //  <Link to='/contact'><li>Contact</li></Link>
